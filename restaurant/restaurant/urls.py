d7098b02e011a7d0841ff345b7c69f73bc195cf8
protik-das-utso <protikdas018830@gmail.com>
2024-12-08 15:30:29 +0600

    add html, static files

--- restaurant/restaurant/urls.py
+++ restaurant/restaurant/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from . import settings
+from django.conf import settings
 from django.urls import path
 from django.conf.urls.static import static
 from Base_App.views import *
@@ -23,9 +23,9 @@ from Base_App.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView),
-    path('/book_table', BookTableView),
-    path('/menu', MenuView),
-    path('/about', AboutView),
+    path('book_table', BookTableView),
+    path('menu', MenuView),
+    path('about', AboutView),
 
 
 ]
